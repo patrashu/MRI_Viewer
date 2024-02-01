48400113b1f6384bf0a0033a99fad723b34911ea
patrashu <qhdrmfdl123naver.com>
2024-02-01 21:55:32 +0900

    Change to darktheme

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import qdarktheme
 from PySide6.QtWidgets import QApplication
 
 from src.main_window import MainWindow
@@ -6,6 +7,7 @@ from src.main_window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    qdarktheme.setup_theme()
     main_window = MainWindow()
     main_window.show()
     app.exec()
